5551ec1e1956e14eddc664af91fa3e7d6f6911b8
Marek Beran <marek.beran@elvac.eu>
2025-09-13 19:28:10 +0200

    Uprava grafu

--- Generators/PlotGenerator.cs
+++ Generators/PlotGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using ReGen.Model;
 using ScottPlot;
+using ScottPlot.TickGenerators;
 
 namespace ReGen.Generators;
 
@@ -19,6 +20,10 @@ public static class PlotGenerator
             .Select(r => r.StepTime.TotalSeconds)
             .ToArray();
 
+        // Přeškálování xs tak, aby začínalo od 0
+        var minXs = xs.Min();
+        var xsRescaled = xs.Select(x => x - minXs).ToArray();
+
         // Parse voltage (remove '+' sign if present)
         var ys = csvRecords
             .Select(r => r.V)
@@ -26,15 +31,35 @@ public static class PlotGenerator
 
         // Create ScottPlot 5 plot
         var plt = new Plot();
-        var scatter = plt.Add.ScatterLine(xs, ys, Colors.Black);
+        var scatter = plt.Add.ScatterLine(xsRescaled, ys, Colors.Black);
         scatter.LineWidth = 5;
-        plt.XLabel("Time (seconds)");
-        plt.YLabel("Voltage (V)");
+        plt.XLabel("Čas");
 
-        // Save to byte[]
-        // using var ms = new MemoryStream();
+// Vytvoření ticků s rozumným intervalem
+        var maxTime = xsRescaled.Max();
+        var tickPositions = new List<double>();
+        var tickLabels = new List<string>();
+
+// Automatické určení intervalu podle délky dat
+        var interval = maxTime switch
+        {
+            <= 60 => 10,      // Do 1 minuty: každých 10 sekund
+            <= 300 => 30,     // Do 5 minut: každých 30 sekund
+            <= 600 => 60,     // Do 10 minut: každou minutu
+            <= 1800 => 180,   // Do 30 minut: každé 3 minuty
+            _ => 300          // Více než 30 minut: každých 5 minut
+        };
+
+        for (double t = 0; t <= maxTime; t += interval)
+        {
+            tickPositions.Add(t);
+            var ts = TimeSpan.FromSeconds((int)t);
+            tickLabels.Add($"{ts.Minutes}:{ts.Seconds:D2}");
+        }
+
+        plt.Axes.Bottom.TickGenerator = new NumericManual(tickPositions.ToArray(), tickLabels.ToArray());
+
+        plt.YLabel("Napětí (V)");
         plt.SavePng(filePath, 800, 800);
-        // plt.SavePng(ms, 800, 600);
-        // return ms.ToArray();
     }
 }
